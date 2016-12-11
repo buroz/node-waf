74646cc2f202454f254a6b91d20fc2402e536144
DanielRuf <daniel@daniel-ruf.de>
2016-12-11 18:50:00 +0100

    improved simple test file, use native timer

--- regex.js
+++ regex.js
@@ -4,15 +4,9 @@ const rules = require('./rules');
 // var t = s.match(/\b(eval)\s*\(/i);
 // console.log(t);
 
-function getNanoSecTime() {
-  var hrTime = process.hrtime();
-  return hrTime[0] * 1000000000 + hrTime[1];
-}
-
-const start = getNanoSecTime();
-
 const attack_string = 'localHost';
 
+console.time("RegEx test");
 for (let rule in rules) {
 	// @todo: testing, testing, testing
 	const current_rule = rules[rule];
@@ -21,14 +15,8 @@ for (let rule in rules) {
 	current_rule.chain[1].where.split('|').indexOf('GET') !== -1 &&
 	attack_string.match(current_rule.chain[1].what) !== null
 	){
-		console.log(current_rule.why);
-		//break;
+		// console.log(current_rule.why);
+		break;
 	}
 }
-
-const end = getNanoSecTime();
-
-console.log(end - start, 'nanoseconds');
-console.log((end - start)/ 1000, 'microseconds');
-console.log((end - start)/ 1000000, 'milliseconds');
-console.log((end - start)/ 1000000000, 'seconds');
+console.timeEnd("RegEx test");
