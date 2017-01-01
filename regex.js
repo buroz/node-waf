583b9d395522f2f7fc1a93f96ca361203efa7f44
DanielRuf <daniel@daniel-ruf.de>
2017-01-01 18:01:28 +0100

    changed test file

--- regex.js
+++ regex.js
@@ -1,15 +1,18 @@
 // testfile for testing some things
+console.time("RegEx test");
+console.time("loadrules test");
 const rules = require('./rules');
+console.timeEnd("loadrules test");
 const attack_string = 'localHost';
 
-console.time("RegEx test");
+
 for (const rule in rules) {
 	// @todo: testing, testing, testing
 	const current_rule = rules[rule];
 	if(
 	current_rule.enable &&
-	current_rule.chain[1].where.split('|').indexOf('GET') !== -1 &&
-	attack_string.match(current_rule.chain[1].what) !== null
+	current_rule.chain[0].where.split('|').indexOf('GET') !== -1 &&
+	attack_string.match(new RegExp(current_rule.chain[0].what, current_rule.chain[0].what_flags!==undefined? current_rule.chain[0].what_flags : "")) !== null
 	){
 		// console.log(current_rule.why);
 		break;
